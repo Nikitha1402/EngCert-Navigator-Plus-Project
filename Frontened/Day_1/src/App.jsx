436c97a7db95b864593dfa98a1ec0ad8fa9f1233
Nikitha1402 <119092964+Nikitha1402@users.noreply.github.com>
2024-01-22 15:00:07 +0530

    Add files via upload

---
+++ Frontened/Day_1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Hello_World from './components/Hello_World'
+
+function App() {
+
+  return (
+    <Hello_World/>
+  
+  )
+}
+
+export default App
---
+++ Frontened/Day_1/src/components/Hello_World.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Hello_World(){
+    return (
+        <div>
+        <hi>NIKHITA
+        </hi></div>
+    )
+}
+export default Hello_World;
